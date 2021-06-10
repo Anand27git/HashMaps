c3dc8a4add4b2cae78a96c63d050a9d61ab3c3e1
Anand27git <anandkumar256k@gmail.com>
2021-06-10 21:35:07 +0530

    Searching for an Element

--- main/java/com/BridgeLabz/binarysearchtree/MyBinaryTree.java
+++ main/java/com/BridgeLabz/binarysearchtree/MyBinaryTree.java
@@ -1,8 +1,8 @@
 package com.BridgeLabz.binarysearchtree;
+
 /*******
  * 
- * @author ANAND
- * purpose: simple  BinaryTree 
+ * @author ANAND purpose: simple BinaryTree
  * @param <K>
  */
 public class MyBinaryTree<K extends Comparable<K>> {
@@ -12,7 +12,7 @@ public class MyBinaryTree<K extends Comparable<K>> {
 	public void add(K key) {
 		this.root = addRecursively(root, key);
 	}
-	//
+
 	private MyBinaryNode<K> addRecursively(MyBinaryNode<K> current, K key) {
 		if (current == null)
 			return new MyBinaryNode<>(key);
@@ -26,12 +26,30 @@ public class MyBinaryTree<K extends Comparable<K>> {
 		}
 		return current;
 	}
-	//getting size of the binary tree nodes
+
+	// getting size of the binary tree nodes
 	public int getSize() {
 		return this.getSizeRecursive(root);
 	}
-     //computation of size
+
+	// computation of size
 	public int getSizeRecursive(MyBinaryNode<K> current) {
 		return current == null ? 0 : 1 + this.getSizeRecursive(current.left) + this.getSizeRecursive(current.right);
 	}
+
+	public boolean search(K key) {
+		return searchNodeInMyBinaryTree(root, key);
+	}
+	// search computation
+	public boolean searchNodeInMyBinaryTree(MyBinaryNode<K> root, K key) {
+		if (root == null)
+			return false;
+		if (root.key.compareTo(key) == 0)
+			return true;
+		else if (root.key.compareTo(key) > 0)
+			return searchNodeInMyBinaryTree(root.left, key);
+		else
+			return searchNodeInMyBinaryTree(root.right, key);
+	}
+
 }
--- test/java/com/BridgeLabz/binarysearchtree/MyBinaryTreeTest.java
+++ test/java/com/BridgeLabz/binarysearchtree/MyBinaryTreeTest.java
@@ -2,18 +2,18 @@ package com.BridgeLabz.binarysearchtree;
 
 import org.junit.Test;
 import org.junit.Assert;
+
 /***********
  * 
  * @author ANAND
- * purpose:test case of Binary tree 
+ *  purpose:test case of Binary tree
  *
  */
 
 
 public class MyBinaryTreeTest {
-
+	
 	//UC -1 testing the code of simple binaryTree of 3 nodes
-
 	@Test
 	public void GivenNumbersWhenAddedToBSTShouldReturnSize() {
 		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
@@ -24,25 +24,48 @@ public class MyBinaryTreeTest {
 		Assert.assertEquals(3, size);
 		System.out.println(size);
 	}
-	//UC -2 testing the code of binaryTree of 13 nodes
+
+	// UC -2 testing the code of binaryTree of 13 nodes
 	@Test
 	public void GivenNumbersWhenAddedToBSTShouldReturnSizeofgivenNodes() {
-			MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
-			myBinaryTree.add(56);
-			myBinaryTree.add(30);
-	        	myBinaryTree.add(70);
-        		myBinaryTree.add(22);
-        		myBinaryTree.add(40);
-        		myBinaryTree.add(60);
-        		myBinaryTree.add(95);
-        		myBinaryTree.add(11);
-        		myBinaryTree.add(65);
-        		myBinaryTree.add(3);
-        		myBinaryTree.add(16);
-        		myBinaryTree.add(63);
-        		myBinaryTree.add(67);
-			int size = myBinaryTree.getSize();
-			Assert.assertEquals(13, size);
-			System.out.println(size);
-		}
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(11);
+		myBinaryTree.add(65);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		int size = myBinaryTree.getSize();
+		Assert.assertEquals(13, size);
+		System.out.println(size);
+	}
+
+	// UC -3 test case of search node of Binary Tree
+	@Test
+	public void GivenNumbersWhenAddedToBSTShouldReturnSizeandSearch() {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(11);
+		myBinaryTree.add(65);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		int size = myBinaryTree.getSize();
+		Assert.assertEquals(true, myBinaryTree.search(40));
+		System.out.println(size);
+	}
 }
